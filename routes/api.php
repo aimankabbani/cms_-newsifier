980e4153ed485ef97ad45efc9a090132228c2693
aimankabbani <aimankabbani@gmail.com>
2022-08-09 17:17:17 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::post('/gif/{term}',[GIFController::class,'load'])->name('gif.search');
 Route::post('/admin/load-users',[UtilsController::class,'loadUsers'])->name('users.search');
